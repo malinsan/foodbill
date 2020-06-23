71e8a37b8515874a6d4ebc1a5c932973e39b1b1f
Pontus Eriksson <pontus_eriksson@live.com>
2020-06-23 16:20:36 +0200

    Starting on layout

--- foodbill/frontend/src/components/App.jsx
+++ foodbill/frontend/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import Button from '@material-ui/core/Button'
+import Header from './layout/Header'
 
 class App extends Component {
     render() {
-        return <Button variant="contained" color="primary">REACT APP</Button>
+        return (<div>
+            <Header />
+        </div>)
     }
 }
 
